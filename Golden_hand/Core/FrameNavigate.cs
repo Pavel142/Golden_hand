fc326df2bf4ba54a8dfc99216b99b82214472e97
) <)@DESKTOP-4N2SDI4>
2022-12-25 16:31:44 +0300

    Добавьте файлы проекта.

---
+++ Golden_hand/Core/FrameNavigate.cs
@@ -0,0 +1,18 @@
+﻿using Golden_hand.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Golden_hand.Core
+{
+    public static class FrameNavigate 
+    {
+      public static Frame FrameObject { get; set; }
+      
+      public static Golden_handEntities DB { get; set; }
+    }
+}
---
+++ Golden_hand/View/LoginPage/LogPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Golden_hand.View.LoginPage
+{
+    /// <summary>
+    /// Логика взаимодействия для Logpage.xaml
+    /// </summary>
+    public partial class Logpage : Page
+    {
+        public Logpage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
